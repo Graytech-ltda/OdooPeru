62e2745766f168943ec4482ad4c867a25e4cdef2
William <william.genoy@gmail.com>
2016-01-12 09:11:54 -0800

    Se agregó la herencia del partner

--- openacademy/openacademy.py
+++ openacademy/openacademy.py
@@ -1,23 +1,5 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Licence AGPL
 
 from openerp import models, fields
 
@@ -38,7 +20,7 @@ class openacademy_session(models.Model):
     start_date  = fields.Date('Date Start')
     seats       = fields.Integer('Seats')
     duration    = fields.Float('Duration')
-    course_id   = fields.Many2one('openacademy.course',string='course')
+    course_id   = fields.Many2one('openacademy.course',string='Course')
     attendee_ids = fields.One2many('openacademy.attendee', 'session_id', string="Attendees")
     instructor_id = fields.Many2one('res.partner',string='Instructor')
 
---
+++ openacademy/partner.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Licence AGPL
+from openerp.osv import  osv, fields
+
+#partner se hereda con la version 7 debido a que el padre esta en v7
+class res_partner(osv.Model):
+    _name = "res.partner"
+    
+    _columns = {
+        'is_instructor': fields.boolean('Is Instructor'),
+        'session_ids': fields.one2many('openacademy.session','instructor_id',string='Sessions'),
+    }
