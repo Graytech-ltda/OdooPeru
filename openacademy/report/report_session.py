c29d97a16003ef9c81e5a7a80f43937d8ab9ed68
Cubic ERP <info@cubicerp.com>
2016-01-15 12:03:33 -0500

    [IMP] SessionReport with parser

--- openacademy/report/report_session.py
+++ openacademy/report/report_session.py
@@ -1,21 +1,23 @@
 # -*- coding: utf-8 -*-
 
-from openerp import models, fields, api
+from openerp import models, api
+
+class SessionReport_print(report_sxw.rml_parse):
+
+    def __init__(self, cr, uid, name, context=None):
+        if context is None:
+            context = {}
+        super(SessionReport_print, self).__init__(cr, uid, name, context=context)
+        self.localcontext.update({
+            'time': time,
+        })
+
 
 
 class SessionReport(models.AbstractModel):
     _name = 'report.openacademy.report_session_document1'
-    
-    @api.multi
-    def render_html(self, data=None):
-        report_obj = self.env['report']
-        report = report_obj._get_report_from_name('openacademy.report_session_document1')
-        docargs = {
-            'doc_ids': self._ids,
-            'doc_model': report.model,
-            'docs': self,
-        }
-        return report_obj.render('openacademy.report_session_document1', docargs)
-
+    _inherit = 'report.abstract_report'
+    _template = 'openacademy.report_session_document1'
+    _wrapped_report_class = SessionReport_print
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
